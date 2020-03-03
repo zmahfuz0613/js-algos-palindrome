66610ea88c228846deddd5d4bcf5b92904902377
Misha Kessler <misha.kessler@generalassemb.ly>
2020-03-03 15:59:07 -0500

    Update palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -1,11 +1,7 @@
-//PALINDROME
-//string may include spaces, you will need to account for that.
-//for e.g., "a man a plan a canal panama" will pass the test even though the spaces are in different places.
-//just use letters and spaces, no other characters
-
 const palindrome = str => {
 
 }
+
 console.log(palindrome("a man a plan a canal panama"))
 
 module.exports = {
