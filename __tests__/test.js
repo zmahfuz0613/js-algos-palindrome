4af779f3a029b1a332b2750561c21803ed7bf8ef
Misha Kessler <misha.kessler@generalassemb.ly>
2020-03-03 16:00:21 -0500

    Update test.js

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,10 +1,7 @@
 let algos = require("../palindrome.js")
 
-
-//PALINDROME
 test("check if word is a palindrome", () => {
   expect(algos.palindrome("eye")).toBe(true)
-  // expect(algos.palindrome("a man a plan a canal panama")).toBe(true)
-
-  // expect(algos.palindrome("eyed")).toBe(false)
+  expect(algos.palindrome("a man a plan a canal panama")).toBe(true)
+  expect(algos.palindrome("eyed")).toBe(false)
 })
