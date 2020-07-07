fd0036ff9dc782dcb94d5b15ee2e07f72552f8cf
Zunayed Mahfuz <zunayedmahfuz@Zunayeds-MacBook-Air.local>
2020-07-07 19:49:50 -0400

    Completed

--- palindrome.js
+++ palindrome.js
@@ -1,6 +1,18 @@
 const palindrome = str => {
+  
+  const stringPal = str.toLowerCase().replace(/\s+/g, '')
 
-}
+  let reverseString = []
+  for (let i = stringPal.length - 1; i >= 0; i--) {
+    reverseString += stringPal[i]
+  }
+
+  for (let j = 0; j < stringPal.length; j++) {
+    return (stringPal[j] === reverseString[j]) ? true : false
+
+  }
+  
+  }
 
 console.log(palindrome("a man a plan a canal panama"))
 
